551de5d7f3d90944f0506f396473c2b336e935bb
eduardo <eduardomachadorezende0@gmail.com>
2021-02-13 09:49:54 -0300

    Atualização

--- src/main/java/com/github/eduumach/pix/entity/Chave.java
+++ src/main/java/com/github/eduumach/pix/entity/Chave.java
@@ -7,14 +7,11 @@ import javax.persistence.*;
 public class Chave {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
     @Column(name = "ID")
-    private Long id;
-
-    @Column(name = "CHAVE")
     private String chave;
 
     @ManyToOne
+    @Column(name = "ID_PESSOA")
     private Pessoa pessoa;
 
     public Chave() {
@@ -23,14 +20,6 @@ public class Chave {
     public Chave(String chave, Pessoa pessoa) {
         this.chave = chave;
         this.pessoa = pessoa;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getChave() {
--- src/main/java/com/github/eduumach/pix/entity/Transferecia.java
+++ src/main/java/com/github/eduumach/pix/entity/Transferecia.java
@@ -1,16 +1,31 @@
 package com.github.eduumach.pix.entity;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.javamoney.moneta.Money;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name ="Transferencia")
 public class Transferecia {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
-    @Column(name = "id")
+    @Column(name = "ID")
     private Long id;
 
+    @ManyToOne
+    @Column(name = "CR_CHAVE")
+    private Chave crChave;
+
+    @ManyToOne
+    @Column(name = "CD_CHAVE")
+    private Chave cdChave;
+
+    @Column(name = "QUANTIDADE")
+    private Money quantidade;
+
+    @Column(name ="HORA_E_DATA")
+    private Date horaeData;
 
 }
